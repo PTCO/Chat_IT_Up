2a15dc4a7e1b07074da936ed987c857b27f8a13d
Brandon Durand <ptco.dev@gmail.com>
2024-06-23 01:37:48 -0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -36,7 +36,7 @@ app.use(expressSession({
   cookie: {
     httpOnly: false,
     secure: true,
-    sameSite: 'strict',
+    sameSite: 'Strict',
     expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
   }
 }));
