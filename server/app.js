d4b0c0a07a04d008e88e1a0d4eb672d21e821c76
Brandon Durand <ptco.dev@gmail.com>
2024-06-23 00:30:13 -0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({
-  origin: 'https://chatitup.vercel.app',
+  "Access-Control-Allow-Origin": "*",
   credentials: true
 }));
 
