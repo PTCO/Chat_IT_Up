d0df91ef5fcc4f83c87c8be9b9d8c8044ad300b6
Brandon Durand <ptco.dev@gmail.com>
2024-06-24 01:35:15 -0500

    Update userContext.js

--- client/src/contex/userContext.js
+++ client/src/contex/userContext.js
@@ -41,12 +41,15 @@ export const UserProvider = (props) => {
         await axios.post(`${process.env.REACT_APP_SERVER_URL}User/SignIn`, {formData: data})
         .then( result => {
             setAuthUser(result.data); 
-            console.log(Cookie.get('connect.sid'), JSON.stringify(Cookie.get('connect.sid')));
-            Cookie.set("userSession", JSON.stringify({s:JSON.stringify(Cookie.get('connect.sid'))}), {secure: true, sameSite: 'None', expires: 7 * 24 * 60 * 60 * 1000})
             navigate('/Chat');
         })
         .catch( errors => {
             handleErrors(errors);
+        })
+        .finally(()=>{
+            setTimeout(() => {
+                Cookie.set("userSession", JSON.stringify({s:JSON.stringify(Cookie.get('connect.sid'))}), {secure: true, sameSite: 'None', expires: 7 * 24 * 60 * 60 * 1000})
+            }, 2000);
         })
     }
 
