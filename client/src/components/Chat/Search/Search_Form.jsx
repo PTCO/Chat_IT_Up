3e2e61fed4e016729d71f96e01a224cb401031f6
Brandon Durand <ptco.dev@gmail.com>
2024-06-25 14:44:22 -0500

    Update Search_Form.jsx

--- client/src/components/Chat/Search/Search_Form.jsx
+++ client/src/components/Chat/Search/Search_Form.jsx
@@ -18,7 +18,7 @@ const Search_Form = () => {
             <form action="" className='d-flex align-items-center w-100' onSubmit={ e => actions.searchSession(e, search.current.value)} >
                 <input type="text" className='form-control fs-6 SearchField' ref={search}/><button type="submit" className='btn btn-dark ms-2 fs-6'>SEARCH</button>
             </form>
-            <div className="d-flex flex-column py-2 border-top border-2 overflow-auto mt-4 h-100  SeachResults" style={accentColor ? { borderColor: accentColor}:null}>
+            <div className="d-flex flex-column py-2 border-top border-2 overflow-auto mt-4 h-100  SeachResults" style={accentColor ? { borderColor: `${accentColor} !important`}:null}>
                 <h3 className={`${resultMsg ? 'd-flex':'d-none'} fs-2 mx-auto mt-4 bg-dark p-2 text-white rounded`}>{resultMsg}</h3>
                 { 
                     results.map( data => (
