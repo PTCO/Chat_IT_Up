928c6e655c2e20a609ce98725e420bd187331dae
Brandon Durand <ptco.dev@gmail.com>
2024-09-20 10:51:33 -0500

    Update CustomForm.jsx

--- client/src/components/Utility/Forms/CustomForm.jsx
+++ client/src/components/Utility/Forms/CustomForm.jsx
@@ -50,22 +50,27 @@ const CustomForm =  ({ inputs , legend, button, Size, Oauth}) => {
                 />
             ))
             }
-            <span className="d-flex align-items-center">
-            {button.map( btn => (
-                <button 
-                    key={btn.title} 
-                    type={btn.type} 
-                    onClick={ e => `${btn.path ? navigate(btn.path) : null}`} 
-                    className="btn btn-dark mt-2 w-50 me-md-2">
-                    { btn.title }
-                </button>
-            ))}
-            </span>
-            <span className={`${Oauth.length > 0 ? 'd-flex':'d-none'} align-items-center mt-2 m`}>
-                {Oauth.length > 0 && Oauth.map(auth => {
-                    return <a key={auth.icon} className="bg-white d-flex me-2  align-items-center " href={`${auth.type === 'google' ? `http://localhost:5000/User/Google`:`http://localhost:5000/User/Twitter`}`}><i className={`fa-brands fa-${auth.icon} fa-2x ${auth.type === 'google' ? 'text-danger':'text-info'}`}></i></a>
-                })}
-            </span>
+            <div className="d-flex">
+                <span className="d-flex align-items-center">
+                {button.map( btn => (
+                    <button 
+                        key={btn.title} 
+                        type={btn.type} 
+                        onClick={ e => `${btn.path ? navigate(btn.path) : null}`} 
+                        className="btn btn-dark mt-2 w-100 me-md-2">
+                        { btn.title }
+                    </button>
+                ))}
+                </span>
+                <span className={`${Oauth.length > 0 ? 'd-flex':'d-none'}  align-items-center mt-2`}>
+                    <h5 className="mb-0 ms-2 ms-sm-3 me-1 me-sm-3 fs-6">Or <b style={{color: "#f8b231"}}>Login</b>  <b style={{color: "rgb(180 128 251)"}}>With</b></h5>
+                    <div className="border border-2 p-1 rounded d-flex">
+                        {Oauth && Oauth.length > 0 && Oauth.map(auth => {
+                            return <a key={auth.icon} className="bg-white d-flex mx-1 mx-md-2  align-items-center " href={`${auth.type === 'google' ? `${REACT_APP_SERVER_URL}User/Google`:`${REACT_APP_SERVER_URL}User/Twitter`}`}><i className={`fa-brands fa-${auth.icon} fa-2x ${auth.type === 'google' ? 'text-danger':'text-info'}`}></i></a>
+                        })}
+                    </div>
+                </span>
+            </div>
             {errors.map( error => (
                 <h3 key={error} className={`${errors.length > 0} mt-2 mb-0 border rounded fs-6 errorMsg`} style={{padding: '.15em'}}>{ error } <i className="fa-solid fa-circle-exclamation text-danger"></i></h3>
             ))}
